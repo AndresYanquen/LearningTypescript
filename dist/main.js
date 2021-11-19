6e2998917a6f99dbcaccc2ad82e3959a935c0390
Andres Yanquen <andres.yanquen@hotmail.com>
2021-11-19 12:30:20 -0500

    Variables

---
+++ dist/main.js
@@ -0,0 +1,7 @@
+"use strict";
+console.log("Hola Platzi Amigos desde main");
+var phone;
+var hex = 0x0fff;
+var binary = 38;
+phone = 23;
+console.log("Your phone is: " + phone);
---
+++ src/main.ts
@@ -0,0 +1,8 @@
+console.log("Hola Platzi Amigos desde main")
+
+let phone: number;
+let hex: number= 0x0fff;
+let binary: number= 0b100110;
+phone = 23;
+
+console.log("Your phone is: "+ phone)
